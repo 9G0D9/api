2b976cfce49046ec270fe2f5a94d8a89a8d11e9b
Ghassen <dhiebghassen@gmail.com>
2023-07-31 18:49:24 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,81 @@
+import spacy
+from spacy.training.example import Example
+import random
+import json
+import pandas as pd
+from pdfminer.high_level import extract_text
+import os
+from flask import Flask, render_template, request, jsonify
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+# Set the upload folder for the uploaded PDF files
+app.config['UPLOAD_FOLDER'] = os.path.abspath('uploads')
+
+output_dir='my_model_30'
+
+
+def make_dict(doc, nlp):
+    # Get all possible entity labels from the model
+    all_entity_labels = nlp.get_pipe("ner").labels
+
+    # Initialize a dictionary to store entities for each label as lists
+    entities_dict = {label: [] for label in all_entity_labels}
+
+    # Extract entities and group them by label
+    for ent in doc.ents:
+        entities_dict[ent.label_].append(ent.text)
+
+    # Convert empty lists to lists containing None for all columns
+    for label in entities_dict:
+        if not entities_dict[label]:
+            entities_dict[label] = [None]
+
+    return entities_dict
+
+
+def convert_pdf(name):
+    text=extract_text(name)
+    return text
+
+def get_output(nlp, name):
+    text = convert_pdf(name)
+    doc = nlp(text)
+    entities_dict = make_dict(doc, nlp)
+    return entities_dict
+
+
+
+@app.route('/process_pdf', methods=['GET', 'POST'])
+def process_pdf():
+    if request.method == 'POST':
+        # Check if the request contains a file with the key 'pdf_file'
+        if 'pdf_file' not in request.files:
+            return jsonify({"error": "No file provided in the request."}), 400
+
+        pdf_file = request.files['pdf_file']
+
+        if pdf_file.filename == '':
+            return jsonify({"error": "No file selected."}), 400
+
+        if pdf_file and allowed_file(pdf_file.filename):
+            filename = secure_filename(pdf_file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            pdf_file.save(file_path)
+            entities_dict = get_output(nlp_model, file_path)
+            os.remove(file_path)
+
+            # Render the result.html template with the entities as context data
+            return render_template('result.html', entities=entities_dict)
+
+    # For GET requests, render the HTML form for manual file upload
+    return render_template('upload.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'pdf'}
+
+if __name__ == '__main__':
+    nlp_model = spacy.load(output_dir)
+    app.run(debug=True)
